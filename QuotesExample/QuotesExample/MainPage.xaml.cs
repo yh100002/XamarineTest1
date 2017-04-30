bd9144f2bf96afb08ad5d3e963bc7eaa26d26502
Young Ho Son <Young Ho Son>
2017-04-30 14:22:13 +0700

    초기 커밋.

---
+++ QuotesExample/QuotesExample/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace QuotesExample
+{
+	public partial class MainPage : ContentPage
+	{
+        private int _index = 0;
+        private List<string> _listText = new List<string>()
+        {
+            "Life is like riding a bicycle. To keep your balance, you must keep moving.",
+            "You can't blame gravity for falling in love.",
+            "Look deep into nature, and then you will understand everything better."
+        };
+
+		public MainPage()
+		{
+			InitializeComponent();
+
+            currentQuote.Text = _listText[0];
+		}
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            //var answer = DisplayAlert("Question?", "Would you like to play a game", "Yes", "No");
+            //System.Diagnostics.Debug.WriteLine("Answer: " + answer);
+
+            if (_index == _listText.Count()) _index = 0;
+
+            currentQuote.Text = _listText[_index++];
+        }
+    }
+}
